e53678193e3587043486725d9f2126aa3cf2923b
mwammth <147476234+mwammth@users.noreply.github.com>
2023-11-21 18:21:25 +0900

    Update calc.js
    条件分岐変更しました。

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,11 +1,11 @@
-let num = Math.floor(Math.random() * 16);
-if (num===3 || num===6 ||num===9 ||num===12) {
+let num = Math.floor(Math.random() * 30);
+if (num%3===0 & num%15!==0) {
   console.log('3の倍数です');
 }
-else if (num===5 || num===10) {
+else if (num%5===0 & num%15!==0) {
   console.log('5の倍数です');
 }
-else if (num===15) {
+else if (num%3===0 & num%5===0) {
   console.log('3と5の倍数です');
 }
 else {
