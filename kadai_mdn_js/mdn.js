115ecc1d3125d73fb6919597e3e1a4c9b7594e0f
mwammth <147476234+mwammth@users.noreply.github.com>
2023-11-22 11:44:19 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const moonLanding = new Date('9 22, 23');
+const yaer = moonLanding.getFullYear()
+const month = moonLanding.getMonth()
+const day = moonLanding.getDate()
+
+console.log(yaer+'年'+month+'月'+day+'日');
